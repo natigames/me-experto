cc2486a59bda6d3945344574bee5f242588d6d67
Juan Carlos Cardoso <papo@nati.games>
2020-05-25 20:34:14 -0500

    initial commit

---
+++ experto/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace experto
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void LoginButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(PassEntry.Text);
+            if (!isEmailEmpty && !isPasswordEmpty)
+            {
+                //do login...
+                Navigation.PushAsync(new HomePage());
+
+            }
+
+        }
+    }
+}
